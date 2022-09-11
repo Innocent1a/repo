ef0954f15852d55544c018770afae9a41b0be733
She Coder <indembera98@gmail.com>
2022-09-11 11:10:19 +0200

    First Commint

---
+++ lib/screens/dialScreen/dial_screen.dart
@@ -0,0 +1,16 @@
+import 'package:videocall/constants.dart';
+import 'package:videocall/size_config.dart';
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class DialScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: kBackgoundColor,
+      body: Body(),
+    );
+  }
+}
